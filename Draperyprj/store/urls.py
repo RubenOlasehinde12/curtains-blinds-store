ebbc5dd79d006059adfcafc1cdd3af8d8eb036f3
Ruben Olasehinde <rubenolasehinde93@gmail.com>
2025-08-16 09:05:25 +0100

    feat(store/search): add product search + URL + navbar form

--- Draperyprj/store/urls.py
+++ Draperyprj/store/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, SearchResultsListView
 
 
 app_name = "store"
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
 
--- Draperyprj/store/views.py
+++ Draperyprj/store/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, DetailView
 from .models import Product
+from django.db.models import Q
 
-# Create your views here.
+# Create your views here
 class ProductListView(ListView):
     model = Product
     context_object_name = "products"
@@ -11,3 +12,14 @@ class ProductDetailView(DetailView):
     model = Product
     context_object_name = "product"
     template_name = 'store/product_detail.html'
+
+class SearchResultsListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'store/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Product.objects.filter(
+            Q(product_name__icontains=query)
+        )
